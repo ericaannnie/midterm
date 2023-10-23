b32b6068d482a94cf8bd2760fc8eb18b702728b1
ericaannnie <147415296+ericaannnie@users.noreply.github.com>
2023-10-23 15:45:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ import seaborn as sns
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn import metrics as mt
-import plotly.express as px
 import streamlit as st
 import random
 from PIL import Image
