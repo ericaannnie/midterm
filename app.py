7fc83912cb1d9a45240e821f7d89ca2c0b67a37a
jfarias123 <117544541+jfarias123@users.noreply.github.com>
2023-10-24 22:18:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -81,9 +81,9 @@ if app_mode == 'Introduction':
     """
 
     # Apply to each column
-    col1.markdown(f'<div style="{box_style}"><strong>Age</strong><br><br><br>The person\'s current age.</div>', unsafe_allow_html=True)
+    col1.markdown(f'<div style="{box_style}"><strong>Age</strong><br><br><br>The age at which the person currently is.</div>', unsafe_allow_html=True)
     col2.markdown(f'<div style="{box_style}"><strong>Gender</strong><br><br><br>Gender of the person. Either male or female.</div>', unsafe_allow_html=True)
-    col3.markdown(f'<div style="{box_style}"><strong>Education Level</strong><br><br>Education level of the professional.<br>Ex: Bachelors</div>', unsafe_allow_html=True)
+    col3.markdown(f'<div style="{box_style}"><strong>Education Level</strong><br><br>Academic education level of the professional.<br>Ex: Bachelors</div>', unsafe_allow_html=True)
     col4.markdown(f'<div style="{box_style}"><strong>Job Title</strong><br><br><br>Title of the professional\'s job.<br>Ex: Project Manager</div>', unsafe_allow_html=True)
     col5.markdown(f'<div style="{box_style}"><strong>Years of Experience</strong><br><br>Number of years in the workforce</div>', unsafe_allow_html=True)
     col6.markdown(f'<div style="{box_style}"><strong>Salary</strong><br><br><br>Salary in USD of the professional.</div>', unsafe_allow_html=True)
